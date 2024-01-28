81a2e754522a94b6f4997cb23771d32f05f67ca4
Murali423 <1murali5teja@gmail.com>
2024-01-28 20:56:52 +0530

    Added setup template

---
+++ Array/End-to-end-MedicalChatBot/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = "Medical Chatbot",
+    version= '0.0.0',
+    author= 'Murali divya Teja',
+    author_email= '1murali5teja@gmail.com',
+    packages= find_packages(),
+    install_requires =[]
+)
